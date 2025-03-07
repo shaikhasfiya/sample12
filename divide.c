fbd025e797d6051c0872e641986bbe48635a7223
Asfiya shaikh <asfiyashaih781@gmail.com>
2025-03-07 22:15:34 +0530

    my sample program

---
+++ divide.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+
+ 
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+
+    if (num2 == 0) {
+        printf("Error! Division by zero is not allowed.\n");
+    } else {
+        result = num1 / num2;
+        printf("Result: %.2f\n", result);
+    }
+
+    return 0;
+}
